7308f340287da63bc951eaab7d07cb0c988f8dc9
Dario <darioandrade@email.arizona.edu>
2019-02-05 23:02:27 -0700

    Added Enemy  behavior script

---
+++ Assets/Scripts/Enemy_Behavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Behavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform Enemy;
+    float enemyTimer = 0f;
+    void Start()
+    {
+        Instantiate(Enemy, new Vector3(-5.5f, 4.5f, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyTimer > 1f) {
+            Instantiate(Enemy, new Vector3(-5.5f, 4.5f, 0), Quaternion.identity);
+            enemyTimer = 0f;
+        }
+        enemyTimer += Time.deltaTime;
+    }
+
+    void FixedUpdate() {
+        
+
+    }
+}
