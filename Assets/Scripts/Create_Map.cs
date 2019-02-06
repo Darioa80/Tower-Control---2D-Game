da031736f671172334b50a9144b97caba9e8a977
Dario <darioandrade@email.arizona.edu>
2019-02-05 23:44:05 -0700

    path attempt

--- Assets/Scripts/Create_Map.cs
+++ Assets/Scripts/Create_Map.cs
@@ -5,14 +5,48 @@ using UnityEngine;
 public class Create_Map : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Transform Enemy;
+    public Transform Path_Tile;
+    float enemyTimer = 0f;
+    public float mapWidth;
+    public float mapHeight;
+
+
     void Start()
     {
-        
+        CreateMap();
+        //Instantiate(Enemy, new Vector3(-5.5f, 4.5f, 0), Quaternion.identity);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (enemyTimer > 0.75f)
+        {
+            Instantiate(Enemy, new Vector3(-4.5f, 4.5f, 0), Quaternion.identity);
+            enemyTimer = 0f;
+        }
+        enemyTimer += Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+
+
+    }
+
+    void CreateMap() {
+        for (float indexWidth = 0f; indexWidth < mapWidth; indexWidth++)
+        {
+            for (float indexHeight = 0f; indexHeight < mapHeight; indexHeight++)
+            {
+                if (indexWidth != -4.5f && indexHeight > 0f)
+                {
+                    Instantiate(Path_Tile, new Vector3(indexWidth - 6.5f, indexHeight - 4.5f, 0), Quaternion.identity);
+                }
+            }
+        }
+
     }
 }
--- Assets/Scripts/Enemy_Behavior.cs
+++ Assets/Scripts/Enemy_Behavior.cs
@@ -9,17 +9,13 @@ public class Enemy_Behavior : MonoBehaviour
     float enemyTimer = 0f;
     void Start()
     {
-        Instantiate(Enemy, new Vector3(-5.5f, 4.5f, 0), Quaternion.identity);
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (enemyTimer > 1f) {
-            Instantiate(Enemy, new Vector3(-5.5f, 4.5f, 0), Quaternion.identity);
-            enemyTimer = 0f;
-        }
-        enemyTimer += Time.deltaTime;
+
     }
 
     void FixedUpdate() {
