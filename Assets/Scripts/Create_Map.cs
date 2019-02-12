c3898189681fafd1440ff66264b0e904073bc63f
Dario <darioandrade@email.arizona.edu>
2019-02-11 23:26:17 -0700

    Collisions occur

--- Assets/Scripts/Create_Map.cs
+++ Assets/Scripts/Create_Map.cs
@@ -14,11 +14,14 @@ public class Create_Map : MonoBehaviour
     public float mapHeight;
     private int count;
 
+    List<GameObject> non_Path_List;
+
     private float last_y_Position = 0f;
     private float last_x_Position = 0f;
 
     void Start()
     {
+        non_Path_List = new List<GameObject>();
         CreateMap();
         //Instantiate(Enemy, new Vector3(-5.5f, 4.5f, 0), Quaternion.identity);
       
@@ -44,6 +47,7 @@ public class Create_Map : MonoBehaviour
                 Temp = Instantiate(Non_Path_Tile, new Vector3(0.5f + width_index, -0.5f - height_index, 0f), Quaternion.identity) as GameObject;
                 Temp.GetComponent<Position>().x_Coordinate = 0.5f + width_index;
                 Temp.GetComponent<Position>().y_Coordinate = -0.5f - height_index;
+                non_Path_List.Add(Temp);
             }
 
         }
@@ -52,37 +56,59 @@ public class Create_Map : MonoBehaviour
 
     void AddPath() {
         for (count = 0; count < 4; count++) {
+            //Instantiate(Path_Tile, new Vector3(1.5f, -0.5f - count, 0f), Quaternion.identity);
             Instantiate(Path_Tile, new Vector3(1.5f, -0.5f - count, 0f), Quaternion.identity);
-            
+            searchList(1.5f, -0.5f - count);
+
         }
         last_y_Position = -0.5f - count + 1;
         last_x_Position = 1.5f;
         for (count = 1; count < 7; count++)
         {
             Instantiate(Path_Tile, new Vector3(last_x_Position + count, last_y_Position, 0f), Quaternion.identity);
-            
+            searchList(last_x_Position + count, last_y_Position);
         }
         last_x_Position = last_x_Position + count;
         for (count = 0; count < 3; count++)
         {
             Instantiate(Path_Tile, new Vector3(last_x_Position, last_y_Position + count, 0f), Quaternion.identity);
+            searchList(last_x_Position, last_y_Position + count);
         }
         last_y_Position = last_y_Position + count -1;
         for (count = 1; count < 5; count++)
         {
             Instantiate(Path_Tile, new Vector3(last_x_Position + count, last_y_Position, 0f), Quaternion.identity);
+            searchList(last_x_Position + count, last_y_Position);
         }
         last_x_Position = last_x_Position + count - 1;
         for (count = 1; count < 6; count++)
         {
-            Instantiate(Path_Tile, new Vector3(last_x_Position, last_y_Position - count, 0f), Quaternion.identity);
+            Temp = Instantiate(Path_Tile, new Vector3(last_x_Position, last_y_Position - count, 0f), Quaternion.identity);
+            searchList(last_x_Position, last_y_Position - count);
         }
         last_y_Position = last_y_Position - count + 1;
         for (count = 1; count < 7; count++)
         {
             Instantiate(Path_Tile, new Vector3(last_x_Position-count, last_y_Position, 0f), Quaternion.identity);
+            searchList(last_x_Position - count, last_y_Position);
         }
         last_x_Position = last_x_Position - count + 1;
         Instantiate(Path_Tile, new Vector3(last_x_Position, last_y_Position-1, 0f), Quaternion.identity);
+        searchList(last_x_Position, last_y_Position - 1);
+    }
+
+    void searchList(float x, float y) {
+        int index = 0;
+        for (index = 0; index < non_Path_List.Count; index++) {
+            if (x == non_Path_List[index].GetComponent<Position>().x_Coordinate) {
+                if (y == non_Path_List[index].GetComponent<Position>().y_Coordinate) {
+                    Destroy(non_Path_List[index]);
+
+                }
+            }
+        }
+
+
+
     }
 }
