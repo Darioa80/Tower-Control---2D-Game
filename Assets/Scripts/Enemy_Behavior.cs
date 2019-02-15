8b1ee96af5d5c9d61960337cdf3934478bc98100
Dario <darioandrade@email.arizona.edu>
2019-02-14 22:11:45 -0700

    messing with raycast

--- Assets/Scripts/Enemy_Behavior.cs
+++ Assets/Scripts/Enemy_Behavior.cs
@@ -11,10 +11,11 @@ public class Enemy_Behavior : MonoBehaviour
     public float horizontalSpeed = 0f;
     public float verticalSpeed = 0f;
     public Collider2D hitCollider;
+    private RaycastHit2D hit;
     void Start()
     {
         verticalSpeed = -(enemySpeed * Time.deltaTime);
-        this.transform.Translate(new Vector3(horizontalSpeed, verticalSpeed, 0f));
+       
     }
 
     // Update is called once per frame
@@ -34,9 +35,10 @@ public class Enemy_Behavior : MonoBehaviour
         Vector2 off = new Vector2(this.transform.position.x+0.5f, this.transform.position.y + 0.5f);
         if (verticalSpeed < 0f)
         {
-            RaycastHit2D hit = Physics2D.Raycast(off, Vector2.right);
+            hit = Physics2D.Raycast(off, Vector2.right);
             hitCollider = hit.collider;
             print("Hit Collider: " + hit.collider);
+            print("Hit distance: " + hit.distance);
             if (hit.distance > 1)
             {
 
@@ -50,9 +52,13 @@ public class Enemy_Behavior : MonoBehaviour
         }
         else if (horizontalSpeed > 0f) {
             Debug.DrawRay(this.transform.position, (new Vector3(this.transform.position.x, this.transform.position.y + 2f, 0f) - this.transform.position) * 2f, Color.yellow);
-            bool test = Physics2D.Raycast(this.transform.position, new Vector3(this.transform.position.x, this.transform.position.y + 2f, 0f) - this.transform.position, 10f);
-            print("test: " + test);
-            if (test)
+            //bool test = Physics2D.Raycast(this.transform.position, new Vector3(this.transform.position.x, this.transform.position.y + 2f, 0f) - this.transform.position, 10f);
+            hit = Physics2D.Raycast(off, Vector2.up);
+            hitCollider = hit.collider;
+            print("Hit Collider: " + hit.collider);
+            print("Hit distance: " + hit.distance);
+           
+            if (hit.distance < 1)
             {
                 verticalSpeed = -horizontalSpeed;   
             }
