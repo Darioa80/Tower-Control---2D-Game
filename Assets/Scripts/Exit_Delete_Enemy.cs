ec3057085666ba95d2cb60cedb6e33fb0c666138
Dario <darioandrade@email.arizona.edu>
2019-03-08 23:02:46 -0700

    Fixed Delete Enemy upon exit

--- Assets/Scripts/Exit_Delete_Enemy.cs
+++ Assets/Scripts/Exit_Delete_Enemy.cs
@@ -8,13 +8,16 @@ using TMPro;
 
 public class Exit_Delete_Enemy : MonoBehaviour
 {
-    public TextMeshProUGUI HealthText;
-    public Player player;
+    private TextMeshProUGUI HealthText;
+    private Player player;
+    private Spawn_Enemies EnemyManager;
+    int toDel;
     // Start is called before the first frame update
     void Start()
     {
         HealthText = GameObject.Find("Health Number").GetComponent<TextMeshProUGUI>();
         player = GameObject.FindWithTag("player").GetComponent<Player>();
+        
     }
 
     // Update is called once per frame
@@ -23,14 +26,27 @@ public class Exit_Delete_Enemy : MonoBehaviour
         
     }
     public void OnTriggerEnter2D(Collider2D col) {
-       
+        EnemyManager = GameObject.FindWithTag("EnemyManager").GetComponent<Spawn_Enemies>();
         if (col.gameObject.CompareTag("Enemy")) {
             player.health = player.health - 1;
             HealthText.text = "" + player.health;
             if (player.health <= 0) {
                 //SceneManager.LoadScene(4);
             }
+            toDel = SearchEnemyList(col.gameObject);
+            EnemyManager.enemyList.Remove(EnemyManager.enemyList[toDel]);
             Destroy(col.gameObject);
         }
+    }
+
+    public int SearchEnemyList(GameObject searchFor) {
+        print("col: " + searchFor);
+        for (int i = 0; i < EnemyManager.enemyList.Count; i++) {
+            print("enemy " + EnemyManager.enemyList[i]);
+            if (searchFor == EnemyManager.enemyList[i]) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
